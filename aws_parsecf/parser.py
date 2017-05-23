69f25be5e02d368481e26127c1851b0273667a39
Oded Niv <oded.niv@gmail.com>
2017-05-23 17:27:37 +0300

    load from file formats, fix Condition objects, unknown Fn::GetAtt

--- aws_parsecf/parser.py
+++ aws_parsecf/parser.py
@@ -4,29 +4,38 @@ import json
 import yaml
 
 def load_json(stream, default_region=boto3.Session().region_name):
-    return _explode(json.load(stream), default_region)
+    return _load(json.load(stream), default_region)
 
 def loads_json(string, default_region=boto3.Session().region_name):
-    return _explode(string.loads(string), default_region)
+    return _load(json.loads(string), default_region)
 
 def load_yaml(stream_or_string, default_region=boto3.Session().region_name):
-    return _explode(yaml.load(stream_or_string), default_region)
+    return _load(yaml.load(stream_or_string), default_region)
 
-def _explode(root, default_region, current=None):
+def _load(root, default_region):
     """
     >>> import json
 
-    >>> print(json.dumps(_explode({
+    >>> print(json.dumps(_load({
     ...     'Conditions': {'ConditionName': {'Fn::Equals': [1, 2]}},
     ...     'Resources': {'SomeResource': {'Condition': 'ConditionName', 'Type': 'AWS::Lambda::Function'}}
     ...     }, 'us-east-1'), sort_keys=True))
     {"Conditions": {"ConditionName": false}, "Resources": {}}
-    >>> print(json.dumps(_explode({
+    >>> print(json.dumps(_load({
+    ...     'Conditions': {'ConditionName': {'Fn::Equals': [1, 1]}},
+    ...     'Resources': {'SomeResource': {'Condition': 'ConditionName', 'Type': 'AWS::Lambda::Function'}}
+    ...     }, 'us-east-1'), sort_keys=True))
+    {"Conditions": {"ConditionName": true}, "Resources": {"SomeResource": {"Condition": "ConditionName", "Type": "AWS::Lambda::Function"}}}
+    >>> print(json.dumps(_load({
+    ...     'Resources': {'SomeResource': {'Condition': {'DateGreaterThan': {'aws:CurrentTime': '2013-12-15T12:00:00Z'}, 'Type': 'AWS::IAM::Role'}}}
+    ...     }, 'us-east-1'), sort_keys=True))
+    {"Resources": {"SomeResource": {"Condition": {"DateGreaterThan": {"aws:CurrentTime": "2013-12-15T12:00:00Z"}, "Type": "AWS::IAM::Role"}}}}
+    >>> print(json.dumps(_load({
     ...     'Conditions': {'ConditionName': {'Fn::Equals': [1, 2]}},
     ...     'Resources': {'SomeResource': {'Attribute': {'Fn::If': ['ConditionName', '1', {'Ref': 'AWS::NoValue'}]}, 'Type': 'AWS::Lambda::Function'}}
     ...     }, 'us-east-1'), sort_keys=True))
     {"Conditions": {"ConditionName": false}, "Resources": {"SomeResource": {"Type": "AWS::Lambda::Function"}}}
-    >>> print(json.dumps(_explode({
+    >>> print(json.dumps(_load({
     ...     'Conditions': {'ConditionName': {'Fn::Equals': [{'Ref': 'SomeBucket'}, 'SomeBucketName']}},
     ...     'Resources':
     ...         {'SomeBucket': {'Properties': {'BucketName': 'SomeBucketName'}, 'Type': 'AWS::S3::Bucket'},
@@ -50,12 +59,14 @@ def _explode(root, default_region, current=None):
         }
     }
     """
-    if current is None:
-        current = root
-        _explode(root, default_region, root)
-        _cleanup(root)
-        return root
 
+    if not default_region:
+        raise TypeError("No default region in aws configuration, please specify one (with `aws configure` or `default_region=`)")
+    _explode(root, default_region, root)
+    _cleanup(root)
+    return root
+
+def _explode(root, default_region, current):
     # object
     if isinstance(current, dict):
         if '_exploded' in current:
@@ -67,7 +78,7 @@ def _explode(root, default_region, current=None):
             _exploded(root, current, key, default_region)
 
         condition_name = current.get('Condition')
-        if condition_name:
+        if condition_name and isinstance(condition_name, str):
             # condition
             if not conditions.evaluate(root, condition_name, default_region):
                 return DELETE
@@ -80,12 +91,13 @@ def _explode(root, default_region, current=None):
                 if e.args != (key,):
                     raise
                 # not an intrinsic function
-            try:
-                return conditions.evaluate(root, {key: value}, default_region)
-            except KeyError as e:
-                if e.args != (key,):
-                    raise
-                # not a condition
+            if key != 'Condition': # 'Condition' means a name of a condtion, would make a mess
+                try:
+                    return conditions.evaluate(root, {key: value}, default_region)
+                except KeyError as e:
+                    if e.args != (key,):
+                        raise
+                    # not a condition
     # array
     elif isinstance(current, list):
         for index, value in enumerate(current):
