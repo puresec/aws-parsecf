c4652e24363de9ada7c5100083d625b60c1cbd26
Oded Niv <oded.niv@gmail.com>
2017-06-19 17:52:14 +0300

    fix setup.py url

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup, find_packages
 
 setup(
-        name='aws-parsecf',
-        version='1.0.0',
-        description="Parse AWS CloudFormation's intrinsic functions in the template",
-        long_description=open('README.rst').read(),
-        author='Oded Niv',
-        url='https://github.com/aws-parsecf',
-        packages=find_packages(exclude=['tests*']),
-        install_requires=[
-            'boto3',
-            ],
-        )
+    name='aws-parsecf',
+    version='1.0.0',
+    description="Parse AWS CloudFormation's intrinsic functions in the template",
+    long_description=open('README.rst').read(),
+    author='Oded Niv',
+    url='https://github.com/puresec/aws-parsecf',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=[
+        'boto3',
+    ],
+)
 
