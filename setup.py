ff8348596de2cf38ead38a7d5fb063bd78c92b1f
Oded Niv <oded.niv@gmail.com>
2017-07-12 11:44:52 +0300

    v1.0.2

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='aws-parsecf',
-    version='1.0.1',
+    version='1.0.2',
     description="Parse AWS CloudFormation's intrinsic functions in the template",
     long_description=open('README.rst').read(),
-    author='Oded Niv <oded.niv@gmail.com>',
+    author='Oded Niv',
+    author_email='oded.niv@gmail.com',
     url='https://github.com/puresec/aws-parsecf',
     packages=find_packages(exclude=['tests*']),
     install_requires=[
