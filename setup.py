3830b87f674adbc34f71553698abcad905facb0d
Rajesh Acharya <sorkan@hotmail.com>
2023-06-11 14:15:37 -0400

    Update setup.py
    update version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aws-parsecf',
-    version='1.1.0',
+    version='1.1.1',
     description="Parse AWS CloudFormation's intrinsic functions in the template",
     long_description=open('README.rst').read(),
     author='Oded Niv',
